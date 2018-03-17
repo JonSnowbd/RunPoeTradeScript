f883c630bc048c063a245fc8d40379d1b5d9c2ea
Josiah P <vasti009@gmail.com>
2018-03-17 09:42:47 -0300

    done

---
+++ Finder.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace BlueholeSucks
+{
+    partial class Finder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusMessage = new System.Windows.Forms.Label();
+            this.RetryButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.IterationLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StatusMessage
+            // 
+            this.StatusMessage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.StatusMessage.AutoSize = true;
+            this.StatusMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusMessage.Location = new System.Drawing.Point(156, 97);
+            this.StatusMessage.Name = "StatusMessage";
+            this.StatusMessage.Size = new System.Drawing.Size(255, 24);
+            this.StatusMessage.TabIndex = 0;
+            this.StatusMessage.Text = "Looking for Path of Exile...";
+            // 
+            // RetryButton
+            // 
+            this.RetryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.RetryButton.Location = new System.Drawing.Point(13, 184);
+            this.RetryButton.Name = "RetryButton";
+            this.RetryButton.Size = new System.Drawing.Size(75, 23);
+            this.RetryButton.TabIndex = 1;
+            this.RetryButton.Text = "Retry";
+            this.RetryButton.UseVisualStyleBackColor = true;
+            this.RetryButton.Click += new System.EventHandler(this.RetryButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.Location = new System.Drawing.Point(450, 175);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(105, 32);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit(and AHK)";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // IterationLabel
+            // 
+            this.IterationLabel.AutoSize = true;
+            this.IterationLabel.Location = new System.Drawing.Point(13, 13);
+            this.IterationLabel.Name = "IterationLabel";
+            this.IterationLabel.Size = new System.Drawing.Size(0, 13);
+            this.IterationLabel.TabIndex = 3;
+            // 
+            // Finder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 219);
+            this.Controls.Add(this.IterationLabel);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.RetryButton);
+            this.Controls.Add(this.StatusMessage);
+            this.Name = "Finder";
+            this.Text = "...";
+            this.Load += new System.EventHandler(this.Finder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StatusMessage;
+        private System.Windows.Forms.Button RetryButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label IterationLabel;
+    }
+}
+
---
+++ Finder.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace BlueholeSucks
+{
+    public partial class Finder : Form
+    {
+        private Timer Count;
+
+        public Finder()
+        {
+            InitializeComponent();
+            string[] Messages =
+            {
+                "Blue 'fun and banned go hand in hand' Hole",
+                "Blue 'run a script and you get ript' Hole",
+                "Blue 'battle eye will make you cry' Hole",
+                "Blue 'bans our fans' Hole",
+                "Blue 'quite the fan? have a ban' Hole",
+                "Blue 'fun with a friend? that will end' Hole"
+            };
+            Random rng = new Random();
+            this.Text = Messages[rng.Next(Messages.Length)];
+        }
+
+        private void Finder_Load(object sender, EventArgs e)
+        {
+            RetryButton.Hide();
+            SetMessage("Looking for Path of Exile Process...");
+            if (PathofExileIsPresent())
+            {
+                Count = new Timer();
+                
+                // Every 3.5 seconds check if POE has closed.
+                // I'd rather poll and be safe than to hook into POE's process and get banned there too...
+                Count.Interval = 3500;
+                Count.Tick += CheckPathofExile;
+
+                Count.Start();
+
+                SetMessage("Found Path of Exile.\nStarting Trade Script");
+
+                try
+                {
+                     Process.Start("Run_TradeMacro.ahk");
+                }
+                catch
+                {
+                    SetMessage("It seems you havent placed this .exe\nNext to Run_TradeMacro.ahk.");
+                    Count.Dispose();
+                    Count = null;
+                    RetryButton.Show();
+                }
+            }
+            else
+            {
+                SetMessage("Could not find Path of Exile. \n Start the game then retry.");
+                RetryButton.Show();
+            }
+                
+        }
+
+        private void SetMessage(string msg)
+        {
+            StatusMessage.Text = msg;
+            StatusMessage.Top = (this.ClientSize.Height - StatusMessage.Height) / 2;
+            StatusMessage.Left = (this.ClientSize.Width - StatusMessage.Width) / 2;
+        }
+
+        private bool PathofExileIsPresent()
+        {
+            Process[] L = Process.GetProcessesByName("PathOfExile");
+            if (L.Length == 0)
+                return false;
+            else
+                return true;
+        }
+
+        // Iters tracks how many polls have been done so far
+        // Checks for path of exile. If it's ever not found(ran from a Timer), disposes everything.
+        // And kills ahk.
+        private int iters = 0;
+        private void CheckPathofExile(object sender, EventArgs e)
+        {
+            if (!PathofExileIsPresent())
+            {
+                Count.Stop();
+                Count.Dispose();
+                KillAHK();
+                Application.Exit();
+                return;
+            }
+            iters++;
+            IterationLabel.Text = "iter " + iters.ToString();
+        }
+
+        // Goes through each instance of ahk and just kills it.
+        private void KillAHK()
+        {
+            try
+            {
+                foreach(Process x in Process.GetProcessesByName("AutoHotkey"))
+                {
+                    x.Kill();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void RetryButton_Click(object sender, EventArgs e)
+        {
+            Finder_Load(sender, e);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            KillAHK();
+            Application.Exit();
+        }
+    }
+}
